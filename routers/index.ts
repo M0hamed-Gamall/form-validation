b20849a44d4778f89c230182df7819f4af4161ac
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2024-12-15 07:49:14 +0200

    fix home page error

--- routers/index.ts
+++ routers/index.ts
@@ -5,7 +5,7 @@ import {validationResult} from 'express-validator'
 const router = Router();
 
 router.get('/' , (req:Request,res:Response,next:NextFunction)=>{
-    res.status(200).render('home')
+    res.status(200).render('home' , {errors:[]})
 })
 
 router.post('/', validate,(req:Request,res:Response,next:NextFunction)=>{
